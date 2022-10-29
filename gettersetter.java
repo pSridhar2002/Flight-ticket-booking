b9e624f252a9ec17dc151b71c3a3381f31328554
Veerender Goud Bommagani <89401262+Veerender123@users.noreply.github.com>
2022-10-29 13:01:52 +0530

    Create gettersetter.java

---
+++ gettersetter.java
@@ -0,0 +1,47 @@
+package pojo;
+
+public class Students {
+	
+	private int sl_no;
+	private String date;
+	private String source;
+	private String dest;
+	private String s;
+	
+	
+	
+
+public int getSl_no() {
+		return sl_no;
+	}
+	public void setSl_no(int sl_no) {
+		this.sl_no = sl_no;
+	}
+public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDest() {
+		return dest;
+	}
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+
+
+
+}
